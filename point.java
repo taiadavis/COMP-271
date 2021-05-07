4c6e2ad0f4943a42252644f87a7e7a0aea717641
taiadavis <64235165+taiadavis@users.noreply.github.com>
2021-05-06 23:16:42 -0500

    Create point.java

---
+++ point.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Point
+{
+  private double x;
+  private double y;
+  
+  public Point ()
+  {
+  	x = 0;
+	y = 0;
+  }
+// -------------------------------------------------------------
+  public Point (double x, double y)
+  {
+	this.x = x;
+	this.y = y;
+  }
+
+
+}
